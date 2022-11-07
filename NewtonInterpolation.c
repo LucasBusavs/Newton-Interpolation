a4182b3d677f3791199bf5f66974a220fb6870ca
Lucas Bueno <turmadaciencia1@gmail.com>
2022-11-07 14:39:30 -0300

    Done

--- NewtonInterpolation.c
+++ NewtonInterpolation.c
@@ -5,13 +5,16 @@
 int main(){
 
     int n;
-    float xp, p, fp = 0;
+    float xp;
+    int j = 1, i;
+    float f1, f2, fp;
 
     printf("Enter amount of data: ");
     scanf("%d", &n);
 
     float x[n];
     float fx[n];
+    float p[n];
 
     for (int i = 0; i<n; i++){
         printf("Enter with x[%d] = ", i);
@@ -19,14 +22,29 @@ int main(){
         printf("Enter with fx[%d] = ", i);
         scanf("%f", &fx[i]);
     }
-
+    
     printf("Enter interpolation point: ");
 	scanf("%f", &xp);
 
-    
+    fp = fx[0];
 
+    do{
+        for (i=0;i<n;i++){
+            p[i] = ((fx[i+1]-fx[i])/(x[i+j]-x[i]));
+            fx[i]=p[i];
+        }
+        f1 = 1;
+        for(i=0;i<j;i++){
+            f1*=(xp-x[i]);
+        }
+        f2+=(fx[0]*f1);
+        n--;
+        j++;
+    }while(n!=1);
+    fp = f2+fp;
 
-    printf("Interpolated value at %.4f is %.4f.", xp, fp);
+    printf("Interpolated value at %.2f is %.2f.", xp, fp);
+    fflush(stdin);
     getchar();
 
     return 0;
